fb95d152fe18c2fdfd2755da695a0242cc7d145f
Keith Capers <K.Capers94@gmail.com>
2023-09-29 22:58:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3 = 53;
+const num4 = 7;
+
+let mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(18,19,20);
+console.log(max)
+
+
